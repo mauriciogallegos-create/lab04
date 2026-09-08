2414a7363442d7c4064d9353b4dd287887f7541e
mauriciogallegos-create <mauricio.gallegos@tecsup.edu.pe>
2026-09-08 16:34:14 -0500

    feat: Add MovieCounter composable function

--- app/src/main/java/com/example/moviecounter/MainActivity.kt
+++ app/src/main/java/com/example/moviecounter/MainActivity.kt
@@ -5,17 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.moviecounter.ui.theme.MovieCounterTheme
 
@@ -55,4 +60,25 @@ fun ComponentsExample(modifier: Modifier = Modifier) {
             Text(text = "+", modifier = Modifier.padding(16.dp))
         }
     }
+}
+
+@Composable
+fun MovieCounter(modifier: Modifier = Modifier) {
+    val count = 0
+    Column(
+        modifier = modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "You have added $count movies.")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { /* Acción del botón */ }) {
+            Text("Add Movie")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewMovieCounter() {
+    MovieCounter()
 }
